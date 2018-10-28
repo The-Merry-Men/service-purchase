cefa836f070b196ac4f59bd2003d16a2aecaaeb2
Alan <al.zhang93@gmail.com>
2018-10-27 18:07:47 -0700

    just some clean up

---
+++ client/src/components/middle-bar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class MiddleBar extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div id='middle-bar'>
+                <div className='share-line'>
+                    <div id='share-text'>Shares</div>
+                    <div>
+                        <form>
+                            <input id='share-input' type='text' value='0'/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MiddleBar;
+
