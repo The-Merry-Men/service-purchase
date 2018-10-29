49eaf2992e53884ea7f8182e2b0de6e2e85f21e9
Alan <al.zhang93@gmail.com>
2018-10-29 14:55:33 -0700

    fixed margins/padding for share input and text

--- client/src/components/middle-bar.jsx
+++ client/src/components/middle-bar.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// const MiddleBarWrapper = styled.div `
-//     border: 1px solid black;
-//     width: 228px;
-// `
-
-
 const MiddleBarWrapper = styled.div `
     border: 1px solid black;
     width: 228px;
@@ -15,20 +9,27 @@ const MiddleBarWrapper = styled.div `
     box-shadow: 1px 1px 10px 1px ${props => props.open ? "whitesmoke" : "black"};
 `
 
+const Text13Div = styled.div `
+    font-size: 13px;
+    font-weight: 400;
+    font-family: "DIN Pro", -apple-system, BlinkMacSystemFont, sans-serif;
+`
+
 const ShareLine = styled.div `
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
 `
-const ShareText = styled.div `
+const ShareText = styled(Text13Div) `
     margin-right: 5px;
-    font-size: 13px;
-    font-weight: 400;
     padding-left: 10px;
-    padding-right: 20px;
-    font-family: "DIN Pro", -apple-system, BlinkMacSystemFont, sans-serif;
+    padding-right: 10px;
     color : ${props => props.open ? "black" : "white"};
+`
+
+const PriceText = styled(ShareText) `
+    font-weight: 600;
 `
 
 const ShareInput = styled.input `
@@ -39,16 +40,15 @@ const ShareInput = styled.input `
     color: ${props => props.open ? "#8c8c8e" : "#8c8c8e"}; 
     border-radius: 2px;
     padding-right: 5px;
-    margin-right: 20px;
+    margin-right: 10px;
     margin-bottom: 4px;
     ${ShareLine}: hover & {
         border-color: ${props => props.open ? "#cbcbcd" : "#8c8c8e"};
     }
 `
 const MarketPriceText = styled(ShareText) `
-    color: ${props => props.up ? "#21ce99" : "#f45531"}; 
-`
-const MarketPrice = styled(ShareText) `
+    color: ${props => props.up ? "#21ce99" : "#f45531"};
+    font-weight: 600; 
 `
 
 class MiddleBar extends React.Component {
@@ -66,7 +66,7 @@ class MiddleBar extends React.Component {
                 </ShareLine>
                 <ShareLine>
                 <MarketPriceText up={this.props.up} >Market Price</MarketPriceText>
-                <MarketPrice open={this.props.open}>${this.props.price}</MarketPrice>
+                <PriceText open={this.props.open}>${this.props.price}</PriceText>
                 </ShareLine>
             </MiddleBarWrapper>
         )
