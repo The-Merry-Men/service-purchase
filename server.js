a411253e7f29ca856982da443c89a4f18ae0073b
Alan <al.zhang93@gmail.com>
2018-10-29 16:47:04 -0700

    fixed syntax issues

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const port = 3001
+const express = require('express');
 
-const app = express()
+const port = 3001;
+const app = express();
 app.listen(port, () => {
-    console.log('server up and running')
-})
+  console.log('server up and running');
+});
 
-app.use('/client', express.static('client'))
-app.use(express.static('public'))
+app.get('/companies/:id', (req, res) => {
+  res.send(req.param('id'));
+});
+
+app.use('/client', express.static('client'));
+app.use(express.static('public'));
