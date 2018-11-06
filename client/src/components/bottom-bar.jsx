c8744212f9c8cbcb46c2862630c67b00c3336146
Alan <al.zhang93@gmail.com>
2018-11-05 16:37:59 -0800

    refactored bottom-bar for increased readability

--- client/src/components/bottom-bar.jsx
+++ client/src/components/bottom-bar.jsx
@@ -6,15 +6,15 @@ const Text13Div = styled.div`
     font-size: 13px;
     font-weight: 400;
     font-family: "DIN Pro", -apple-system, BlinkMacSystemFont, sans-serif;
-    color: ${props => props.open ? 'black' : 'white'};
+    color: ${props => (props.open ? 'black' : 'white')};
 `;
 
 const BottomBarWrapper = styled.div`
     border: 1px solid black;
     width: 228px;
-    background: ${props => props.open ? 'white' : '#1b1b1d'};
-    border: 1px solid ${props => props.open ? 'rgb(224, 224, 224)' : 'black'};
-    box-shadow: 1px 1px 10px 1px ${props => props.open ? 'whitesmoke' : 'black'};
+    background: ${props => (props.open ? 'white' : '#1b1b1d')};
+    border: 1px solid ${props => (props.open ? 'rgb(224, 224, 224)' : 'black')};
+    box-shadow: 1px 1px 10px 1px ${props => (props.open ? 'whitesmoke' : 'black')};
     padding-top: 10px;
     padding-bottom: 10px;
 `;
@@ -32,19 +32,20 @@ const CenterLine = styled(ShareLine)`
 
 class BottomBar extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
+
   render() {
+    const { open, balance } = this.props;
     return (
-      <BottomBarWrapper open={this.props.open} >
+      <BottomBarWrapper open={open}>
         <CenterLine>
-          <Text13Div open={this.props.open}>
-            ${this.props.balance.toFixed(2)}
-            Buying Power Available
+          <Text13Div open={open}>
+            ${balance.toFixed(2)} Buying Power Available
           </Text13Div>
         </CenterLine>
       </BottomBarWrapper>
-    )
+    );
   }
 }
 
